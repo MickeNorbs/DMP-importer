4e8ff60ac2683748397fffb88908061655bed5d4
helena.norback <helena.norback@vibrato.com.au>
2019-04-22 13:16:32 +1000

    loop.py

---
+++ loop.py
@@ -0,0 +1,12 @@
+# importing "array" for array operations 
+import os
+
+# Set variables
+image_dir='/home/ec2-user/environment/image'
+
+# Create an array containing all file names in image directory
+#imagelist = os.listdir(image_dir)
+
+# Remove all names that don't end in .tiff
+#imagelist.remove('fakefile.tf')
+#print(imagelist)
