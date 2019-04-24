39987a69ddf0246c6bcd836cc598ace87d5b6242
helena.norback <helena.norback@vibrato.com.au>
2019-04-24 16:38:58 +1000

    refactor get_workspace into 1 loop

--- loop.py
+++ loop.py
@@ -1,30 +1,39 @@
-# importing "array" for array operations 
 import os
+from sys import platform
 
-def get_files(): 
-	# Set variables
+# Get operating system
+def get_os():
+	if platform == "linux" or platform == "linux2":
+	    # linux
+	    get_workspace()
+	elif platform == "darwin":
+	    # OS X
+	    get_workspace()
+	elif platform == "win32":
+	    # Windows...
+	    get_workspace()
+
+# Get working directory
+def get_workspace(): 
+	# Create the working directory by prompting the user for folders in the directory list
 	cwd = os.getcwd()
 	workspace = cwd+'/shows/'
 
-	print("which show are you working on?")
-	showlist = os.listdir(workspace)
-	print('\n'.join(map(str, showlist)))
-	show = input()
-	workspace = workspace+show+'/'
+	
+	directory = {
+		"show" : None,
+		"shot" : None,
+		"sequence" : None
+	}
 
-	print("which shot are you working on?")
-	shotlist = os.listdir(workspace)
-	print('\n'.join(map(str, shotlist)))
-	shot = input()
-	workspace = workspace+shot+'/'
-
-	print("which sequence are you working on?")
-	sequence = os.listdir(workspace)
-	print('\n'.join(map(str, sequence)))
-	sequence = input()
-	workspace = workspace+sequence
+	for key in directory:
+		print("Which "+key+"?")
+		item = os.listdir(workspace)
+		print('\n'.join(map(str, item)))
+		directory[key] = input()
+		workspace = workspace+directory[key]+'/'
 
 	print(workspace)
 
-get_files()
+get_os()
 
